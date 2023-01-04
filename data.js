b7c9dafe33fc5323f389debe9e33d5347e7fc7fc
Anshika-0309 <97392474+Anshika-0309@users.noreply.github.com>
2023-01-05 02:56:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,297 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "/images/hoodie_main.png",
+      title: "WINTER SALE IS LIVE!!",
+      desc: "DON'T COMPROMISE ON STYLE! FLAT 30% OFF FOR MNNIT STUDENTS",
+      bg: "f5fafd",
+      hgt: "85%",
+      mrg: "10px",
+      pad: "20px",
+    },
+    {
+      id: 2,
+      img: "/images/fast_food_main.png",
+      title: "DON'T MISS: BUY 2 GET 1",
+      desc: "GET A FREE COKE ON ORDERS ABOVE 299/-",
+      bg: "fcf1ed",
+      hgt: "65%",
+      mrg: "6rem",
+    },
+    {
+      id: 3,
+      img: "/images/sports_main.png",
+      title: "SPECIAL OFFER!!",
+      desc: "FREE BADMINTON KIT WORTH 799/- ON ORDERS ABOVE 2999/-",
+      bg: "fbf0f4",
+      hgt: "65%",
+      mrg: "7rem",
+      pad: "1rem",
+      aln: "center"
+    },
+  ];
+
+export const categories = [
+    {
+      id: 1,
+      img: "/images/clothing.png",
+      title: "AWESOME SWAGS!",
+    },
+    {
+      id: 2,
+      img: "/images/stationery.png",
+      title: "ALL YOU MAY NEED",
+    },
+    {
+      id: 3,
+      img: "/images/food.jpg",
+      title: "HUNGRY?",
+    },
+];
+
+export const stationeryProducts = [
+    {
+      id:1,
+      img:"/images/StationeryItem1.png",
+    },
+    {
+      id:2,
+      img:"/images/StationeryItem5.png",
+    },
+    {
+      id:3,
+      img:"/images/StationeryItem3.jpg",
+    },
+    {
+      id:4,
+      img:"https://images-na.ssl-images-amazon.com/images/I/71%2Bp2ElWi2L._AC_SL1000_.jpg",
+    },
+    {
+      id:5,
+      img:"/images/StationeryItem4.png",
+    },
+    {
+      id:6,
+      img:"https://tse4.mm.bing.net/th?id=OIP.ZIEkqB-nfGVDAsvNSiltvwHaHa&pid=Api&P=0",
+    },
+    {
+      id:7,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51EJirBX6bL.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.htconline.in/images/thumbs/0015904_omega-mini-drafter.jpeg",
+    },
+    {
+      id:9,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71bnFBTEHpL._SL1500_.jpg",
+    },
+    {
+      id:10,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41dzgAlYOvL.jpg",
+    },
+    {
+      id:11,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/612f+d0WVxL._SL1080_.jpg",
+    },
+    {
+      id:12,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81QAYRibhCL._SL1500_.jpg",
+    },
+    {
+      id:13,
+      img:"/images/StationeryItem7.webp",
+    },
+    {
+      id:14,
+      img:"https://5.imimg.com/data5/DC/FD/MY-13969640/professional-color-paper-500x500.jpg",
+    },
+    {
+      id:15,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81YYxoDYpmL._SL1500_.jpg",
+    },
+  ]
+
+  export const swagsProducts = [
+    {
+      id:1,
+      img:"/images/hoodie_main.png",
+    },
+    {
+      id:2,
+      img:"/images/My project.png",
+    },
+    {
+      id:3,
+      img:"/images/tshirt2.png",
+    },
+    {
+      id:4,
+      img:"/images/tshirt3.png",
+    },
+    {
+      id:5,
+      img:"/images/hoodie.png",
+    },
+    {
+      id:6,
+      img:"/images/tshirt.png",
+    },
+    {
+      id:7,
+      img:"/images/NIT ALLAHABAD.png",
+    },
+    {
+      id:8,
+      img:"/images/tshirt4.png",
+    },
+  ]
+
+  export const sportsProducts = [
+    {
+      id:1,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41OUCQC-twL.jpg",
+    },
+    {
+      id:2,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/715sckxhAHL._SX679_.jpg",
+    },
+    {
+      id:3,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71vPVbNl8WL._SL1500_.jpg",
+    },
+    {
+      id:4,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81KSXD4oNwL._SX522_.jpg",
+    },
+    {
+      id:5,
+      img:"https://5.imimg.com/data5/AJ/WR/MY-44813490/sports-shuttlecock-500x500.jpg",
+    },
+    {
+      id:6,
+      img:"https://contents.mediadecathlon.com/p1592998/k$c901b69179d40a0c2d013d8880a994f7/sq/Beginner-Boxing-Gloves-100-Red.jpg",
+    },
+    {
+      id:7,
+      img:"https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/819oL6tleuL._SX679_.jpg",
+    },
+    {
+      id:8,
+      img:"https://tse2.mm.bing.net/th?id=OIP.ZwIRZRtesYatKZxTRs77iwHaHZ&pid=Api&P=0",
+    },
+    {
+      id:9,
+      img:"https://www.hartsport.com.au/images/CategoryImages/SX0213.jpg",
+    },
+    {
+      id:10,
+      img:"http://www.directsportseshop.co.uk/Images/ExtraLarge/head_ti_s6_tennis_racket_black.jpg",
+    },
+  ]
+
+  export const foodProducts = [
+    {
+      id:1,
+      img:"https://www.designindaba.com/sites/default/files/node/news/23566/sonic-burger.jpg",
+    },
+    {
+      id:2,
+      img:"https://www.newwaysproducts.com/wp-content/uploads/2018/12/13-1-524x566.jpg",
+    },
+    {
+      id:3,
+      img:"https://tse2.mm.bing.net/th?id=OIP.8E3Y5hQGAGu6aDaZTs4EDwHaHb&pid=Api&P=0",
+    },
+    {
+      id:4,
+      img:"https://holycowvegan.net/wp-content/uploads/2017/03/easy-aloo-paratha-3.jpg",
+    },
+    {
+      id:5,
+      img:"http://images.media-allrecipes.com/userphotos/960x960/5177852.jpg",
+    },
+    {
+      id:6,
+      img:"https://tse4.mm.bing.net/th?id=OIP.fanZPy9rtueJs2yW6jQDqgAAAA&pid=Api&P=0",
+    },
+    {
+      id:7,
+      img:"https://recipefairy.com/wp-content/uploads/2020/07/hard-boiled-eggs.jpg",
+    },
+    {
+      id:8,
+      img:"https://recipes.timesofindia.com/thumb/60057435.cms?imgsize=619324&width=800&height=800",
+    },
+    {
+      id:9,
+      img:"https://www.giftjaipur.com/image/cache/catalog/products%20images/chocolate-pastries-(4-pcs)-500x500.jpg",
+    },
+    {
+      id:10,
+      img:"https://sweetandsavorymeals.com/wp-content/uploads/2020/04/egg-fried-rice-recipe-facebook.jpg",
+    },
+    {
+      id:11,
+      img:"https://recipes.timesofindia.com/thumb/53859488.cms?imgsize=150956&width=800&height=800",
+    },
+    {
+      id:12,
+      img:"https://tse2.mm.bing.net/th?id=OIP.VzJNn4mFPB7mpDe5NGg0twHaGI&pid=Api&P=0",
+    },
+    {
+      id:13,
+      img:"https://5.imimg.com/data5/PM/UG/MY-17923575/amul-lassi.jpg",
+    },
+    {
+      id:14,
+      img:"https://americanogoodies.com/wp-content/uploads/2019/09/HersheysChoclateMilk8oz236ml-1.jpg",
+    },
+    {
+      id:15,
+      img:"https://tse1.mm.bing.net/th?id=OIP.lUoajTljx8c86aQYTVa94QHaHa&pid=Api&P=0",
+    },
+  ]
+
+  export const miscellaneousProducts = [
+    {
+      id:1,
+      img:"https://tse1.mm.bing.net/th?id=OIP.ST0jLDoscIsxiESt5vIDhwHaHW&pid=Api&P=0",
+    },
+    {
+      id:2,
+      img:"https://cdn2.bigcommerce.com/n-biq04i/ivmp9iza/products/15787/images/45243/8711700633987_1024x1024__93670.1481018393.1280.1280.jpg?c=2",
+    },
+    {
+      id:3,
+      img:"https://tse2.mm.bing.net/th?id=OIP.IpG_pOwUhV6EXZ6MAY5YTwHaHa&pid=Api&P=0",
+    },
+    {
+      id:4,
+      img:"https://tse4.mm.bing.net/th?id=OIP.lV0eBFQtn0xTN4xiuAwhfgHaHa&pid=Api&P=0",
+    },
+    {
+      id:5,
+      img:"https://i5.walmartimages.com/asr/1688c109-840b-45b1-80a4-32f0b2c3ec1e.ace002f868b28084337177badec305c8.png",
+    },
+    {
+      id:6,
+      img:"https://tse1.mm.bing.net/th?id=OIP.DzikxTMWZXKZC_VCo1xbCgHaHa&pid=Api&P=0",
+    },
+    {
+      id:7,
+      img:"https://tse1.mm.bing.net/th?id=OIP.yiW5LIj9vbC-nDQzR4zdrQHaHa&pid=Api&P=0",
+    },
+    {
+      id:8,
+      img:"https://tse2.mm.bing.net/th?id=OIP._7-yUapqNqv96DMubsvYigHaHa&pid=Api&P=0",
+    },
+    {
+      id:9,
+      img:"https://tse3.mm.bing.net/th?id=OIP.mknsPHLGbsYSbOUp_VyNVgHaMn&pid=Api&P=0",
+    },
+    {
+      id:10,
+      img:"https://images.homedepot-static.com/productImages/b1bfa7bb-7836-4787-ae5c-efb1a39d75b6/svn/velcro-brand-picture-mirror-hanging-vel-30106-usa-64_1000.jpg",
+    },
+  ]
