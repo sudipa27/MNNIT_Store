ca17b706fedb1cbb02367f4ac899ed5cd68c01b7
Anshika-0309 <97392474+Anshika-0309@users.noreply.github.com>
2023-01-05 02:55:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Item from "./pages/Item";
+import Register from "./pages/Register";
+import Miscellaneous from "./pages/Miscellaneous";
+import Stationery from "./pages/Stationery";
+import Food from "./pages/Food";
+import Swags from "./pages/Swags";
+import Sports from "./pages/Sports";
+
+
+const App=()=>{
+    return <Sports/>;
+};
+export default App;
